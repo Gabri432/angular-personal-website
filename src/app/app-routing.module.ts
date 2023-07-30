b823a110de2258118025f71e055c736417245c39
Gabri432 <gabrielegatti432@gmail.com>
2023-07-30 15:23:36 +0200

    Defining routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { MyProjectsComponent } from './routes/my-projects/my-projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, title: 'About Gabriele Gatti'},
+  {path: 'projects', component: MyProjectsComponent, title: 'My Projects'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
