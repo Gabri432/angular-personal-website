515adda072e8869ac614d97d47e4628f9269699e
Gabri432 <gabrielegatti432@gmail.com>
2023-07-30 16:22:42 +0200

    Styling home component.

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  englishAboutMe: string = "Hi there! I'm an italian aspiring software developer currently studying computer science at university.";
+  italianAboutMe: string = "Ciao! Sono un aspirante sviluppatore di software che attualmente studia Informatica all'Università!";
+
+  englishPassions: string[] = [
+    "Basketball", "Olympic Lifting", "Physics", "Math", "Artificial Intelligence", 
+    "Web Development", "App Development", "Game development"
+  ];
+  italianPassions: string[] = [
+    "Pallacanestro", "Olympic Lifting", "Fisica", "Matematica","Intelligenza Artificiale", 
+    "Sviluppo web", "Sviluppo di applicazioni", "Sviluppo di giochi"
+  ];
+
+  englishSkills: string[] = [
+    "HTML", "CSS", "JavaScript",
+    "Angular", "Python", "Java", "Github", "English", "Italian"
+  ];
+  italianSkills: string[] = [
+    "HTML", "CSS", "JavaScript",
+    "Angular", "Python", "Java", "Github", "Inglese", "Italiano"
+  ];
+
 }
