6e599815b1b252e18e4da9de77e430bc5ea17449
Gabri432 <gabrielegatti432@gmail.com>
2023-08-04 20:27:50 +0200

    Creating 'ProjectDisplayerComponent' and using it on 'MyProjectsComponent'.

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { ProjectDisplayerComponent } from './my-projects/sub-components/project-displayer/project-displayer.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    MyProjectsComponent
+    MyProjectsComponent,
+    ProjectDisplayerComponent
   ],
   imports: [
     CommonModule
