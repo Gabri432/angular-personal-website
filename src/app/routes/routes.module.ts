d6cd335acc564be76e4228bd378924e833c39b20
Gabri432 <gabrielegatti432@gmail.com>
2023-08-30 20:59:10 +0200

    Creating the 'blog' component.

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { MyProjectsComponent } from './my-projects/my-projects.component';
 import { ProjectDisplayerComponent } from './my-projects/sub-components/project-displayer/project-displayer.component';
+import { BlogComponent } from './blog/blog.component';
 
 
 
@@ -10,7 +11,8 @@ import { ProjectDisplayerComponent } from './my-projects/sub-components/project-
   declarations: [
     HomeComponent,
     MyProjectsComponent,
-    ProjectDisplayerComponent
+    ProjectDisplayerComponent,
+    BlogComponent
   ],
   imports: [
     CommonModule
