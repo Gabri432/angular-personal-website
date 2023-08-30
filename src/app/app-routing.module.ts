d9efac74a3c1f609f0ad961ba996c69aba8b5d71
Gabri432 <gabrielegatti432@gmail.com>
2023-08-30 21:01:06 +0200

    Defining a route for 'blog' component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
 import { MyProjectsComponent } from './routes/my-projects/my-projects.component';
+import { BlogComponent } from './routes/blog/blog.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'About Gabriele Gatti'},
   {path: 'projects', component: MyProjectsComponent, title: 'My Projects'},
+  {path: 'blog', component: BlogComponent, title: 'My Blogs'},
   {path: '**', component: HomeComponent},
 ];
 
