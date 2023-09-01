1ff503315288b7053496fd3fe694054d0a7e7bb4
Gabri432 <gabrielegatti432@gmail.com>
2023-09-01 22:08:34 +0200

    Defining a route for 'BlogDisplayerComponent'.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
 import { MyProjectsComponent } from './routes/my-projects/my-projects.component';
 import { BlogComponent } from './routes/blog/blog.component';
+import { BlogDisplayerComponent } from './routes/blog/sub-components/blog-displayer/blog-displayer.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'About Gabriele Gatti'},
   {path: 'projects', component: MyProjectsComponent, title: 'My Projects'},
+  {path: 'blogs', component: BlogDisplayerComponent, title: 'My Blogs'},
   {path: 'blog', component: BlogComponent, title: 'My Blogs'},
   {path: '**', component: HomeComponent},
 ];
