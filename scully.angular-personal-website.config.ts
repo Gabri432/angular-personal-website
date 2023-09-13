e96f70e70a752f9d3a9411ce9c32dc25bb251a1d
Gabri432 <gabrielegatti432@gmail.com>
2023-09-13 16:14:57 +0200

    Updating prod distribution.

--- scully.angular-personal-website.config.ts
+++ scully.angular-personal-website.config.ts
@@ -22,7 +22,7 @@ export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'angular-personal-website',
   distFolder: './dist/angular-personal-website', // output directory of your Angular build artifacts
-  outDir: './docs/angular-personal-website', // directory for scully build artifacts
+  outDir: './docs', // directory for scully build artifacts
   defaultPostRenderers: [],
   routes: {},
   extraRoutes: [
