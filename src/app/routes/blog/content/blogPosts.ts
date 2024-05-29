6f2f0275066ed118555bcd3f50314fb7edcce002
Gabri432 <gabrielegatti432@gmail.com>
2024-05-29 20:31:26 +0200

    Selecting better keywords.

--- src/app/routes/blog/content/blogPosts.ts
+++ src/app/routes/blog/content/blogPosts.ts
@@ -96,7 +96,7 @@ export const blogs: Array<BlogPost> = [
       title: "What programming language you should learn",
       description: "What programming language is the best for you? What if you pick the wrong language? Is it easy or hard? I'm going to break down all the steps that will lead to the right choice.",
       author: "Gabriele Gatti",
-      topics: ["programming", "languages", "learn"],
+      topics: ["programming", "programming languages", "learn programming"],
       link: "/blog/programming/what-programming-language-you-should-learn",
       creationDate: "2024-05-25",
       lastUpdateDate: "2024-05-25"
